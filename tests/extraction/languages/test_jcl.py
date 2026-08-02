f5a6b45cbfb7320db6ed246900c5961a25fcb412
Joe Esquibel <squid-protocol@users.noreply.github.com>
2026-08-01 22:34:57 -0400

    Remove unused import in test_jcl.py

--- tests/extraction/languages/test_jcl.py
+++ tests/extraction/languages/test_jcl.py
@@ -16,7 +16,6 @@ if _EXTRACTION_DIR not in sys.path:
 
 from _extraction_harness import (  # noqa: E402 # type: ignore
     assert_invalid_no_match,
-    assert_pathological_match,
     assert_redos_immune,
     assert_valid_dependency_match,
     assert_valid_match,
